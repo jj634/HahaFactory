b72b92ecb2912cff809db204bbfd43ff3a9cecf5
Cathy <xinycath@gmail.com>
2020-04-26 02:20:42 -0400

    incorporated special parsing :)

--- app/jokes/controllers/cos_sim.py
+++ app/jokes/controllers/cos_sim.py
@@ -2,12 +2,17 @@ from nltk.tokenize import TreebankWordTokenizer
 from . import *
 import math
 
-treebank_tokenizer = TreebankWordTokenizer()
-def fast_cossim (query, inverted_index, tokenizer=treebank_tokenizer):
+
+def inv_idx_trad(inverted_index):
+    return {x['term']: [(x['joke_ids'][i], x['tfs'][i]) for i in range(len(
+        x['joke_ids']))] for x in inverted_index}
+
+
+def fast_cossim(query, inv_idx_terms, inverted_index):
     """
         Search the collection of documents for the given query
         Inputs: 
-            query: string 
+            query: list of tokens
             inverted_index: dictionary that matches token to a list of joke_ids
                 - term
                 - list of joke_ids
@@ -16,21 +21,20 @@ def fast_cossim (query, inverted_index, tokenizer=treebank_tokenizer):
 
         Output: list of tuples tuple = (joke, sim_score)
     """
-    result = {} #dictionary mapping doc_id to cosine similarity measure
-    q_list = tokenizer.tokenize(query.lower())
-    q_set = set(q_list)
-    
-    q_norm = 0 #query norm
-    inv_idx_terms = {x['term']:[(x['joke_ids'][i], x['tfs'][i]) for i in range(len(x['joke_ids']))] for x in inverted_index} #dictionary mapping term to list of tuple(joke_id, tf)
-    idf = {} #dictionary mapping term to idf
+    result = {}  # dictionary mapping doc_id to cosine similarity measure
+    q_set = set(query)
+
+    q_norm = 0  # query norm
+    # dictionary mapping term to list of tuple(joke_id, tf)
+    idf = {}  # dictionary mapping term to idf
     for t_dict in inverted_index:
-        if t_dict['term'] in q_set: 
+        if t_dict['term'] in q_set:
             if 'idf' in t_dict.keys():
                 idf[t_dict['term']] = t_dict['idf']
 
     for q_word in q_set:
         if q_word in idf:
-            tf_q = q_list.count(q_word) 
+            tf_q = query.count(q_word)
             q_norm += (tf_q * idf[q_word])**2
             for tup in inv_idx_terms[q_word]:
                 doc = tup[0]
@@ -40,7 +44,7 @@ def fast_cossim (query, inverted_index, tokenizer=treebank_tokenizer):
 
     q_norm = math.sqrt(q_norm)
     for doc in result:
-        norm = Joke.query.filter_by(id = doc).first().norm
+        norm = Joke.query.filter_by(id=doc).first().norm
         result[doc] = result[doc] / (q_norm * float(norm))
 
-    return sorted(result.items(), key = lambda x : (x[1], x[0]), reverse = True )
+    return sorted(result.items(), key=lambda x: (x[1], x[0]), reverse=True)

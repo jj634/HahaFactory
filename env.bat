3bd5f25f6109c3099887a973f54a738f4c1976f7
Winice Hui <winice.hui@gmail.com>
2020-04-27 09:00:25 -0700

    the code literally just wants to whatever the flippity flip it wants today

--- env.bat
+++ env.bat
@@ -3,5 +3,5 @@ SET APP_SETTINGS=config.DevelopmentConfig
 :: Set the DB url to a local database for development
 REM SET DATABASE_URL=postgresql://localhost/hahadata
 SET DATABASE_URL=postgresql://winice:password@localhost:5432/hahadata
-SET PGUSER = 'winice'
-SET PGPASSWORD = 'password'
+SET PGUSER=winice
+SET PGPASSWORD=password

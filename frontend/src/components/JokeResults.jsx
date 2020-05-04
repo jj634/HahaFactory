be9c142a363ffa9f4a2a0b54d683ffe6ce2e1132
suinjung <sj575@cornell.edu>
2020-05-03 22:48:07 -0400

    donut breakdown done

--- frontend/src/components/JokeResults.jsx
+++ frontend/src/components/JokeResults.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Icon, Label, Popup, Button } from 'semantic-ui-react'
 import Highlighter from "react-highlight-words";
 import Rating from '@material-ui/lab/Rating';
+import { Doughnut } from 'react-chartjs-2';
 
 
 const JokeResults = ({ jokes, query }) => {
@@ -33,18 +34,48 @@ const JokeResults = ({ jokes, query }) => {
               </Label>)}
               <div>
               <Popup content={joke.score} position='right center' trigger={
-                <h6 className="doughnut">Joke Rating: 
-                  <Rating className="rating_stars" name="half-rating-read" defaultValue={joke.score} precision={0.05} readOnly />
-                </h6>
+                  <Rating className="rating_stars" name="half-rating-read" defaultValue={joke.score} precision={0.1} readOnly />
               }/>
-              </div>
-              
-              <Popup position='right center' trigger={<h6 className="sim_sc_display">Similarity Score: {joke.similarity}</h6>}>
-                <Popup.Header>Here are the details!</Popup.Header>
+          </div>
+
+              <h6 className="sim_sc_display">Similarity Score: {Number((Number(joke.similarity)*100).toFixed(1))+ "%"}</h6>
+              <Popup position='right center' trigger={<Icon className="info_icon" color='teal' name='question circle' size='large' />} hoverable>
+                <Popup.Header>Similarity Score Breakdown</Popup.Header>
                   <Popup.Content>
-                    <h6 className="doughnut">Keywords Weight (cosine): {joke.cos_score}</h6>
-                    <h6 className="doughnut">Categories Weight (jaccard): {joke.jac_score}</h6>
-                    <h6 className="doughnut">Score Weight: {joke.sc_score}</h6>
+                    <Doughnut
+                    data={
+                      {
+                        labels: ['Keywords (%)', 'Categories (%)', 'Funny Factor (%)'],
+                        datasets: [
+                          {
+                            label: 'breakdown',
+                            backgroundColor: [
+                              '#FDC144',
+                              '#FD6585',
+                              '#3DA3E8'
+                            ],
+                            hoverBackgroundColor: [
+                              '#FEDB93',
+                              '#FEBCCA',
+                              '#3DCEE8'
+                            ],
+                            data: [
+                                    Number((Number(joke.cos_score)/Number(joke.similarity)*100).toFixed(1)), 
+                                    Number((Number(joke.jac_score)/Number(joke.similarity)*100).toFixed(1)), 
+                                    Number((Number(joke.sc_score)/Number(joke.similarity)*100).toFixed(1))
+                                  ]
+                          } 
+                        ]
+                      }
+                    }
+                    options={{
+                      legend:{
+                        display:true,
+                        position:'right',
+                        fontSize: 4
+                      }
+                    }}
+                    />
                   </Popup.Content>
               </Popup>
           </div>
